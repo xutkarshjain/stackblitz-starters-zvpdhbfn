ee993960e80e11e8244dc34c1a5101c706b32314
Utkarsh Jain <xutkarshjain@gmail.com>
2025-08-13 06:00:06 +0530

    Added Scroll

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,33 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { Library } from '../library/library';
-import { addBigData } from '../utils/big-data';
+import { Component, OnInit } from "@angular/core";
+import { Library } from "../library/library";
+import { addBigData } from "../utils/big-data";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   imports: [Library],
-  templateUrl: './app.html',
-  styleUrl: './app.scss',
+  templateUrl: "./app.html",
+  styleUrl: "./app.scss",
 })
 export class App implements OnInit {
   lib = {
-    categories: ['Performance', 'Investments', 'Operations'],
+    categories: ["Performance", "Investments", "Operations"],
     applets: [
       {
-        name: 'Performance Snapshot',
-        categories: ['Performance'],
+        name: "Performance Snapshot",
+        categories: ["Performance"],
       },
       {
-        name: 'Commitment Widget',
-        categories: ['Investments'],
+        name: "Commitment Widget",
+        categories: ["Investments"],
       },
       {
-        name: 'CMS',
-        categories: ['Investments', 'Performance'],
+        name: "CMS",
+        categories: ["Investments", "Performance"],
       },
     ],
   };
 
   ngOnInit(): void {
-    addBigData(this.lib, 100, 5000);
+    addBigData(this.lib, 2000, 5000);
   }
 }
